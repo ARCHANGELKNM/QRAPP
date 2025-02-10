0b838068a77ca8053cb8479bbe423509f68f9362
ARCHANGELKNM <kanyamuraarchangel644@gmail0.com>
2025-02-10 21:23:36 +0200

    commi number 3

--- app/Generator/Scanner/page.js
+++ app/Generator/Scanner/page.js
@@ -1,47 +1,26 @@
-"use client";  
+"use client";
 
-import ScannerCom from "./components/ScannerCom";
-import React, { useEffect, useRef } from "react";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
 
-const App = () => {
-  const scannerRef = useRef(null);
-
-  useEffect(() => {
-    // Check for camera access
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((stream) => {
-        // Stream was successful
-      })
-      .catch((error) => {
-        console.error("Error accessing camera: ", error);
-      });
-  }, []);
-
-  const handleScan = (result) => {
-    if (result) {
-      console.log(result);
-    }
-  };
-
-  const handleError = (error) => {
-    console.error("Error accessing camera: ", error);
-  };
+const Test = (props) => {
+  const [data, setData] = useState("No result");
 
   return (
-    <div className="flex items-center justify-center h-screen ml-4 mt-4">
-      <div className="w-72 h-72" ref={scannerRef}>
-        {" "}
-        {/* Adjust the width and height as needed */}
-        <Scanner
-          onScan={handleScan}
-          onError={handleError}
-          delay={500} // Adjust the delay to 500 milliseconds
-        />
-      </div>
-    </div>
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
+    </>
   );
 };
-
-export default App;
