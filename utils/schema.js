fb40c24a285bf45b8f31c97027b6c03e57def301
ARCHANGELKNM <kanyamuraarchangel644@gmail0.com>
2025-06-26 22:01:08 +0200

    commit and push

--- utils/schema.js
+++
@@ -1,7 +0,0 @@
-// schema.ts
-import { mysqlTable, varchar, int } from "drizzle-orm/mysql-core";
-
-export const users = mysqlTable("users", {
-  id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }),
-});
